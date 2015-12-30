200d62918891e4036dff25d2fe643d42959a88a3
Jessey Eagan <jesseyeagan@gmail.com>
2015-12-29 20:05:37 -0700

    Progress on front end

---
+++ Public/js/countdown.js
@@ -0,0 +1,29 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date('06/07/2016');
+initializeClock('clockdiv', deadline);
--- Public/js/main.js
+++ Public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('se7enfastApp', ['ui.bootstrap']);
+angular.module('se7enfastApp', ['ui.bootstrap''ngRoute']);
 
 angular.module('se7enfastApp').controller('mainController', ['$scope', function($scope,) {
 
