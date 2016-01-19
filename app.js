aa82c44ee22542bf2825e680fee8fd09e7ac65b2
JesseyEagan1 <jessey.eagan@gmail.com>
2016-01-19 12:50:49 -0700

    Procfile

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/submit', function(req, res) {
 //    console.log(json);
 //});
 // Creating Server and Listening for Connections \\
-var port = 80;
+var port = 3000;
 app.listen(port, function(){
     console.log('Server running on port ' + port);
 
