9f13dcc3a819e88ad3f2fd73417f797ec95e95e7
JesseyEagan1 <jessey.eagan@gmail.com>
2016-02-05 11:13:38 -0700

    added disclaimer and copyright info

--- Public/js/main.js
+++ Public/js/main.js
@@ -12,38 +12,40 @@ angular.module('se7enfastApp')
                     url: "",
                     templateUrl: "html/views/home.html"
                 })
-                //.state('/main', {
-                //    url: "",
-                //    templateUrl: "html/views/home.html"
-                //})
+
                 .state('home', {
                     url: "/home",
                     templateUrl: "html/views/home.html"
                 })
+
                 .state('about', {
                     url: "/about",
                     templateUrl: "html/views/about.html"
                 })
+
                 .state('howto', {
                     url: "/howto",
                     templateUrl: "html/views/howto.html"
                 })
+
                 .state('locate', {
                     url: "/locate",
                     templateUrl: "html/views/construction.html"
                 })
+
                 .state('register', {
                     url: "/register",
                     templateUrl: "html/views/register.html"
                 })
+
                 .state('connect', {
                     url: "/connect",
                     templateUrl: "html/views/contact.html"
                 })
-                //.state('contactSuccess', {
-                //    url: "/contactSuccess",
-                //    templateUrl: "html/views/contactSuccess.html"
-                //})
+                .state('disclaimer', {
+                    url: "/theLegalStuff",
+                    templateUrl: "html/views/disclaimer.html"
+                })
         }
     ]);
 
